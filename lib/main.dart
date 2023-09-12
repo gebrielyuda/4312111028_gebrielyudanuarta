6a953fb591488ccaeeda252525c620c7744a7bfa
gebrielyuda <gebrielyuda@gmail.com>
2023-09-12 18:08:47 +0700

    Navigator Pushnamed

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
-import 'ui_view/PageTwo.dart'; 
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) { 
-return MaterialApp(
-title: 'Flutter Demo',
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-home: MyHomePage(),
-);
-}
-}
-class MyHomePage extends StatelessWidget { 
-@override
-Widget build(BuildContext context) {
+import 'ui_view/PageTwo.dart';
 
-return Scaffold( 
-appBar: AppBar(
-title: Text("Navigator"),
-),
-body: Row(
-mainAxisAlignment: MainAxisAlignment.center, 
-children: <Widget>[
-MaterialButton(
-color: Colors.yellow, 
-child: Text("Page 2"), 
-onPressed: () {
-// dibuat berikutnya
-Navigator.push(context, MaterialPageRoute( 
-builder: (context) => PageTwo()
-)
-);
-},
-),
-],
-)
-);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      routes: <String, WidgetBuilder>{
+        '/page2': (BuildContext context) => PageTwo(),
+      },
+    );
+  }
 }
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Navigator"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              color: Colors.yellow,
+              child: Text("Page 2"),
+              onPressed: () {
+                // dibuat berikutnya
+                Navigator.of(context).pushNamed('/page2');
+              },
+            ),
+          ],
+        ));
+  }
 }
