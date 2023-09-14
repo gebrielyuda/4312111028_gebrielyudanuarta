667a6b950829c5de4fcf698c80ba33baa594f888
gebrielyuda <gebrielyuda@gmail.com>
2023-09-14 22:41:42 +0700

    Latihan Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,39 @@
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
-
-GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-void main() => runApp(
-      MaterialApp(
-        builder: FToastBuilder(),
-        home: MyApp(),
-        navigatorKey: navigatorKey,
-      ),
-    );
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState(); 
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Toast"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 24.0,
-          ),
-          ElevatedButton(
- onPressed: () {
- // Langkah 3
- Fluttertoast.showToast(
- msg: "THE toast message",
- toastLength: Toast.LENGTH_SHORT,
- timeInSecForIosWeb: 1,
- backgroundColor: Colors.black,
- textColor: Colors.white,
- fontSize: 16.0,
- );
- },
- child: const Text(
- 'Show Toast Message',
- style: TextStyle(fontSize: 24),
+void main() => runApp(const SnackBarDemo());
+class SnackBarDemo extends StatelessWidget {
+ const SnackBarDemo({super.key});
+ @override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ title: 'SnackBar Demo',
+ home: Scaffold(
+ appBar: AppBar(
+ title: const Text('SnackBar Demo'),
  ),
-),
-
-        ],
-      ),
-    );
-  }
+ body: const SnackBarPage(),
+ ),
+ );
+ }
+}
+class SnackBarPage extends StatelessWidget {
+ const SnackBarPage({super.key});
+ @override
+ Widget build(BuildContext context) {
+ return Center(
+ child: ElevatedButton(
+ onPressed: () {
+ final snackBar = SnackBar(
+ content: const Text('Yay! A SnackBar!'),
+ action: SnackBarAction(
+ label: 'Undo',
+ onPressed: () {
+ },
+ ),
+ );
+ ScaffoldMessenger.of(context).showSnackBar(snackBar);
+ },
+ child: const Text('Show SnackBar'),
+ ),
+ );
+ }
 }
