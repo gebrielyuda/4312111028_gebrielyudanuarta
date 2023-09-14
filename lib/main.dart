8502e5b934dc808427ed5390bbe3a6f1169f54b8
gebrielyuda <gebrielyuda@gmail.com>
2023-09-14 17:41:29 +0700

    Memutar Butterfly Video

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,93 @@
-import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:video_player/video_player.dart';
+void main() => runApp(const VideoPlayerApp());
+class VideoPlayerApp extends StatelessWidget {
+ const VideoPlayerApp({super.key});
+ @override
+ Widget build(BuildContext context) {
+ return const MaterialApp(
+ title: 'Video Player Demo',
+ home: VideoPlayerScreen(),
+ );
+ }
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: AccessCameraPage(),
-    );
-  }
+class VideoPlayerScreen extends StatefulWidget {
+ const VideoPlayerScreen({super.key});
+ @override
+ State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
 }
-
-  class AccessCameraPage extends StatefulWidget {
-    @override
-    _AccessCameraPageState createState() => _AccessCameraPageState();
-}
-
-  class _AccessCameraPageState extends State<AccessCameraPage> {
-   File? _image;
-   //digunakan untuk menampung image
-
-        Future<void> openCamera() async { //fuction openCamera();
-          final pickedImage =
-            await ImagePicker().pickImage(source: ImageSource.camera);
-
-          setState(() { // tempat untuk set state image
-              _image = File(pickedImage!.path);
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+ late VideoPlayerController _controller;
+ late Future<void> _initializeVideoPlayerFuture;
+ @override
+ void initState() {
+ super.initState();
+ // Create and store the VideoPlayerController. The VideoPlayerController
+ // offers several different constructors to play videos from assets, files,
+ // or the internet.
+ // ignore: deprecated_member_use
+ _controller = VideoPlayerController.network(
+ 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+ );
+ // Initialize the controller and store the Future for later use.
+ _initializeVideoPlayerFuture = _controller.initialize();
+ // Use the controller to loop the video.
+ _controller.setLooping(true);
+ }
+ @override
+ void dispose() {
+ // Ensure disposing of the VideoPlayerController to free up resources.
+ _controller.dispose();
+ super.dispose();
+ }
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+ appBar: AppBar(
+ title: const Text('Butterfly Video'),
+ ),
+ // Use a FutureBuilder to display a loading spinner while waiting for the
+ // VideoPlayerController to finish initializing.
+ body: FutureBuilder(
+ future: _initializeVideoPlayerFuture,
+ builder: (context, snapshot) {
+ if (snapshot.connectionState == ConnectionState.done) {
+ // If the VideoPlayerController has finished initialization, use
+ // the data it provides to limit the aspect ratio of the video.
+ return AspectRatio(
+ aspectRatio: _controller.value.aspectRatio,
+ // Use the VideoPlayer widget to display the video.
+ child: VideoPlayer(_controller),
+ );
+ } else {
+ // If the VideoPlayerController is still initializing, show a
+ // loading spinner.
+ return const Center(
+ child: CircularProgressIndicator(),
+ );
+ }
+ },
+ ),
+ floatingActionButton: FloatingActionButton(
+ onPressed: () {
+ // Wrap the play or pause in a call to `setState`. This ensures the
+ // correct icon is shown.
+ setState(() {
+ // If the video is playing, pause it.
+ if (_controller.value.isPlaying) {
+ _controller.pause();
+ } else {
+ // If the video is paused, play it.
+ _controller.play();
+ }
  });
-}
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-      appBar: AppBar(
-        title: Text("Access Camera"),
-      ),
-      body: Container(
-          child: Center(
-            child: _image == null ? Text("No Image") : Image.file(_image!),
-          ),
-        ), 
-        floatingActionButton: FloatingActionButton(
-            child: Icon(
-            Icons.add_a_photo,
-            color: Colors.white,
-          ),
-            backgroundColor: Colors.green,
-            onPressed: () {
-            openCamera();
-          }),
-    );
-  }
+ },
+ // Display the correct icon depending on the state of the player.
+ child: Icon(
+ _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+ ),
+ ),
+ );
+ }
 }
